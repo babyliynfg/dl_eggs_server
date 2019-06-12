8898d9ff8cd9866acf85d745bc779fa8bdf9741d
Li Yuanfeng <278688386@qq.com>
2019-06-12 16:20:52 +0800

    no message

--- application/models/Systemrun_model.php
+++ application/models/Systemrun_model.php
@@ -10,10 +10,6 @@ class Systemrun_model extends MY_Model{
     public function make_eggs($all_miner, $dict)
     {
         $coin_single0 = 0.0003;
-        $coin_single1 = 0.0003 * 1.1;
-        $coin_single2 = 0.0003 * 1.2;
-        $coin_single3 = 0.0003 * 1.3;
-        $coin_single4 = 0.0003 * 2;
 
         $ore_type_id = 1;
 
@@ -28,12 +24,8 @@ class Systemrun_model extends MY_Model{
                 continue;
 
             $nums0 = ($item['fuli'] + $item['dy_fuli']) * $coin_single0;
-            $nums1 = $item['da_fuli'] * $coin_single1;
-            $nums2 = $item['luhua_fuli'] * $coin_single2;
-            $nums3 = $item['gujv_fuli'] * $coin_single3;
-            $nums4 = $item['zhandou_fuli'] * $coin_single4;
 
-            $nums = $nums0 + $nums1 + $nums2 + $nums3 + $nums4;
+            $nums = $nums0;
 
             $sql .= '(' . "'" . $item['uid'] . "'" . ',' . $ore_type_id . ',' . $nums . ',' . $time . '),';
         }
