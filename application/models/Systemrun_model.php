e0e37575b78b2918e03345ccf2222f2cc95f9190
Li Yuanfeng <278688386@qq.com>
2019-06-17 16:56:09 +0800

    no message

--- application/models/Systemrun_model.php
+++ application/models/Systemrun_model.php
@@ -15,25 +15,28 @@ class Systemrun_model extends MY_Model{
 
         $time = time();
 
-        $sql = "INSERT INTO wakuang(`uid`,`ore_type_id`,`ore_num`,`find_time`) VALUES ";
-        foreach ($all_miner as $k => $item) {
+        if (count($all_miner) > 0)
+        {
+            $sql = "INSERT INTO wakuang(`uid`,`ore_type_id`,`ore_num`,`find_time`) VALUES ";
+            foreach ($all_miner as $k => $item) {
 
-            $uid = $item['uid'];
+                $uid = $item['uid'];
 
 //            if (isset($dict[$uid]) && $dict[$uid] >= 15)
 //                continue;
 
-            $nums0 = ($item['fuli'] + $item['dy_fuli']) * $coin_single0;
+                $nums0 = ($item['fuli'] + $item['dy_fuli']) * $coin_single0;
 
-            $nums = $nums0;
+                $nums = $nums0;
 
-            $sql .= '(' . "'" . $uid . "'" . ',' . $ore_type_id . ',' . $nums . ',' . $time . '),';
-        }
-        $sql = rtrim($sql, ',');
+                $sql .= '(' . "'" . $uid . "'" . ',' . $ore_type_id . ',' . $nums . ',' . $time . '),';
+            }
+            $sql = rtrim($sql, ',');
 
 //        echo '<<< '.$sql.' >>>';
 
-        $this->db->query($sql);
+            $this->db->query($sql);
+        }
     }
 }
 
